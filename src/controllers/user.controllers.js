32fef93b711bc2ab6057fb9de609e58d790689c8
jorge chirinos <jorgeanciel29@hotmail.com>
2023-05-07 15:11:29 -0500

    finish

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,43 @@
+const catchError = require("../utils/catchError");
+const User = require("../models/User");
+
+const getAll = catchError(async (req, res) => {
+  // Operaciones...
+  const users = await User.findAll();
+  return res.json(users); /* valor a retornar */
+});
+
+const create = catchError(async (req, res) => {
+  const { first_name, last_name, email, password, birthday } = req.body;
+  const user = await User.create({
+    first_name,
+    last_name,
+    email,
+    password,
+    birthday,
+  });
+  return res.status(201).json(user);
+});
+
+const remove = catchError(async (req, res) => {
+  const { id } = req.params;
+  await User.destroy({ where: { id: id } });
+  return res.sendStatus(204);
+});
+
+const update = catchError(async (req, res) => {
+  const { id } = req.params;
+  const { first_name, last_name, email, password, birthday } = req.body;
+  const user = await User.update(
+    { first_name, last_name, email, password, birthday },
+    { where: { id }, returning: true }
+  );
+  return res.json(user[1][0]);
+});
+
+module.exports = {
+  getAll,
+  create,
+  remove,
+  update,
+};
